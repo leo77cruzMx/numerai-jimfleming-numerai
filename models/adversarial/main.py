9ab76f1dc379c5aa28d4783b7c4abce517f63b00
altermarkive <altermarkive@gmail.com>
2018-01-12 09:32:45 +0100

    Enabling TensorBoard visualization of embeddings generated by the auto encoder and the adversarial network

--- models/adversarial/main.py
+++ models/adversarial/main.py
@@ -22,6 +22,10 @@ from sklearn.utils import shuffle
 
 import os
 
+from PIL import Image
+from tensorflow.contrib.tensorboard.plugins import projector
+
+
 FLAGS = tf.app.flags.FLAGS
 
 tf.app.flags.DEFINE_integer('num_epochs', 30, "")
@@ -43,67 +47,96 @@ def main(_):
 
     X_test = df_test[feature_cols].values
 
+    dimensions = (5, 5)
+    single = 5
+    sprites = [None] * 2
+    sprites[0] = [
+        [1, 1, 1, 1, 1],
+        [1, 1, 1, 1, 1],
+        [1, 0, 0, 0, 1],
+        [1, 1, 1, 1, 1],
+        [1, 1, 1, 1, 1]
+    ]
+    sprites[1] = [
+        [1, 1, 1, 1, 1],
+        [1, 1, 0, 1, 1],
+        [1, 1, 0, 1, 1],
+        [1, 1, 0, 1, 1],
+        [1, 1, 1, 1, 1]
+    ]
+    sprites[0] = Image.fromarray(np.uint8(sprites[0]) * 0xFF)
+    sprites[1] = Image.fromarray(np.uint8(sprites[1]) * 0xFF)
+    count = X_train.shape[0]
+    size = int(math.ceil(math.sqrt(count)))
+    image = Image.new('1', (size * single, size * single))
+    logdir = os.path.join(
+        os.getenv('STORING'),
+        'logs',
+        'adversarial_{}'.format(int(time.time())))
+    os.makedirs(logdir, exist_ok=True)
+    handle = open(os.path.join(logdir, 'metadata.tsv'), 'wb')
+    for i in range(count):
+        location = ((i % size) * single, (i // size) * single)
+        label = int(y_train[i])
+        image.paste(sprites[label], location)
+        handle.write(b'1\n' if label == 1 else b'0\n')
+    handle.close()
+    image.save(os.path.join(logdir, 'sprites.png'))
+
     num_features = len(feature_cols)
     features = tf.placeholder(tf.float32, shape=[None, num_features], name='features')
     targets = tf.placeholder(tf.int32, shape=[None], name='targets')
 
-    with tf.variable_scope('adversarial'):
-        train_model = Model(num_features, features, targets, is_training=True)
+    with tf.name_scope('training'):
+        with tf.variable_scope('adversarial'):
+            train_model = Model(num_features, features, targets, is_training=True)
 
-    with tf.variable_scope('adversarial', reuse=True):
-        test_model = Model(num_features, features, targets, is_training=False)
+    with tf.name_scope('evaluation'):
+        with tf.variable_scope('adversarial', reuse=True):
+            test_model = Model(num_features, features, targets, is_training=False)
 
     summary_op = tf.summary.merge_all()
-    logdir = os.path.join(os.getenv('STORING'), 'logs', 'adversarial_{}'.format(int(time.time())))
     supervisor = tf.train.Supervisor(logdir=logdir, summary_op=None)
     with supervisor.managed_session() as sess:
-        summary_writer = tf.summary.FileWriter(logdir)
-
         print('Training model with {} parameters...'.format(train_model.num_parameters))
         optimize_d, optimize_g = True, True
         with tqdm(total=FLAGS.num_epochs) as pbar:
             for epoch in range(FLAGS.num_epochs):
+                summary_writer = tf.summary.FileWriter(logdir, sess.graph)
+
                 X_train_epoch, y_train_epoch = shuffle(X_train, y_train)
-                num_batches = len(y_train_epoch) // FLAGS.batch_size
 
                 losses_d, losses_g = [], []
-                for batch_index in range(num_batches):
-                    batch_start = batch_index * FLAGS.batch_size
-                    batch_end = batch_start + FLAGS.batch_size
+                if optimize_d:
+                    _, loss_d = sess.run([
+                        train_model.train_step_d,
+                        train_model.loss_d,
+                    ], feed_dict={
+                        features: X_train_epoch,
+                        targets: y_train_epoch,
+                    })
+                else:
+                    loss_d = sess.run(train_model.loss_d, feed_dict={
+                        features: X_train_epoch,
+                        targets: y_train_epoch,
+                    })
 
-                    X_train_batch = X_train_epoch[batch_start:batch_end]
-                    y_train_batch = y_train_epoch[batch_start:batch_end]
+                if optimize_g:
+                    _, loss_g = sess.run([
+                        train_model.train_step_g,
+                        train_model.loss_g,
+                    ], feed_dict={
+                        features: X_train_epoch,
+                        targets: y_train_epoch,
+                    })
+                else:
+                    loss_g = sess.run(train_model.loss_g, feed_dict={
+                        features: X_train_epoch,
+                        targets: y_train_epoch,
+                    })
 
-                    if optimize_d:
-                        _, loss_d = sess.run([
-                            train_model.train_step_d,
-                            train_model.loss_d,
-                        ], feed_dict={
-                            features: X_train_batch,
-                            targets: y_train_batch,
-                        })
-                    else:
-                        loss_d = sess.run(train_model.loss_d, feed_dict={
-                            features: X_train_batch,
-                            targets: y_train_batch,
-                        })
-
-                    if optimize_g:
-                        _, loss_g = sess.run([
-                            train_model.train_step_g,
-                            train_model.loss_g,
-                        ], feed_dict={
-                            features: X_train_batch,
-                            targets: y_train_batch,
-                        })
-                    else:
-                        loss_g = sess.run(train_model.loss_g, feed_dict={
-                            features: X_train_batch,
-                            targets: y_train_batch,
-                        })
-
-                    losses_d.append(loss_d)
-                    losses_g.append(loss_g)
+                losses_d.append(loss_d)
+                losses_g.append(loss_g)
 
                 loss_train_d = np.mean(losses_d)
                 loss_train_g = np.mean(losses_g)
@@ -127,8 +160,6 @@ def main(_):
                 pbar.update()
 
         summary_writer.add_graph(sess.graph)
-        summary_writer.flush()
-        summary_writer.close()
 
         loss_valid_d, loss_valid_g, summary_str = sess.run([
             test_model.loss_d,
@@ -144,7 +175,27 @@ def main(_):
         z_valid = sess.run(test_model.z, feed_dict={ features: X_valid })
         z_test = sess.run(test_model.z, feed_dict={ features: X_test })
 
+        summary_writer.flush()
+        summary_writer.close()
+
         np.savez(os.path.join(os.getenv('STORING'), 'adversarial.npz'), z_train=z_train, z_valid=z_valid, z_test=z_test)
+
+    tf.reset_default_graph()
+    tf.Graph().as_default()
+    embedding_variable = tf.Variable(z_train, name='adversarial_embedding')
+    summary_writer = tf.summary.FileWriter(logdir)
+    config = projector.ProjectorConfig()
+    embedding = config.embeddings.add()
+    embedding.tensor_name = embedding_variable.name
+    embedding.metadata_path = os.path.join(logdir, 'metadata.tsv')
+    embedding.sprite.image_path = os.path.join(logdir, 'sprites.png')
+    embedding.sprite.single_image_dim.extend(dimensions)
+    projector.visualize_embeddings(summary_writer, config)
+    session = tf.InteractiveSession()
+    session.run(tf.global_variables_initializer())
+    saver = tf.train.Saver()
+    saver.save(session, os.path.join(logdir, 'model.ckpt'), 0)
+
 
 if __name__ == "__main__":
     tf.app.run()
--- models/autoencoder/main.py
+++ models/autoencoder/main.py
@@ -23,6 +23,10 @@ from model import Model
 
 import os
 
+from PIL import Image
+from tensorflow.contrib.tensorboard.plugins import projector
+
+
 FLAGS = tf.app.flags.FLAGS
 
 tf.app.flags.DEFINE_integer('num_epochs', 30, "")
@@ -45,43 +49,72 @@ def main(_):
     X_valid = df_valid[feature_cols].values
     X_test = df_test[feature_cols].values
 
+    Y_train = df_train['target'].values
+    dimensions = (5, 5)
+    single = 5
+    sprites = [None] * 2
+    sprites[0] = [
+        [1, 1, 1, 1, 1],
+        [1, 1, 1, 1, 1],
+        [1, 0, 0, 0, 1],
+        [1, 1, 1, 1, 1],
+        [1, 1, 1, 1, 1]
+    ]
+    sprites[1] = [
+        [1, 1, 1, 1, 1],
+        [1, 1, 0, 1, 1],
+        [1, 1, 0, 1, 1],
+        [1, 1, 0, 1, 1],
+        [1, 1, 1, 1, 1]
+    ]
+    sprites[0] = Image.fromarray(np.uint8(sprites[0]) * 0xFF)
+    sprites[1] = Image.fromarray(np.uint8(sprites[1]) * 0xFF)
+    count = X_train.shape[0]
+    size = int(math.ceil(math.sqrt(count)))
+    image = Image.new('1', (size * single, size * single))
+    logdir = os.path.join(os.getenv('STORING'), 'logs', 'autoencoder_{}'.format(int(time.time())))
+    os.makedirs(logdir, exist_ok=True)
+    handle = open(os.path.join(logdir, 'metadata.tsv'), 'wb')
+    for i in range(count):
+        location = ((i % size) * single, (i // size) * single)
+        label = int(Y_train[i])
+        image.paste(sprites[label], location)
+        handle.write(b'1\n' if label == 1 else b'0\n')
+    handle.close()
+    image.save(os.path.join(logdir, 'sprites.png'))
+
     num_features = len(feature_cols)
     features = tf.placeholder(tf.float32, shape=[None, num_features], name='features')
 
-    with tf.variable_scope('autoencoder'):
-        train_model = Model(num_features, features, denoise=FLAGS.denoise, is_training=True)
+    with tf.name_scope('training'):
+        with tf.variable_scope('autoencoder'):
+            train_model = Model(num_features, features, denoise=FLAGS.denoise, is_training=True)
 
-    with tf.variable_scope('autoencoder', reuse=True):
-        test_model = Model(num_features, features, denoise=FLAGS.denoise, is_training=False)
+    with tf.name_scope('evaluation'):
+        with tf.variable_scope('autoencoder', reuse=True):
+            test_model = Model(num_features, features, denoise=FLAGS.denoise, is_training=False)
 
     best = None
     wait = 0
     summary_op = tf.summary.merge_all()
-    logdir = os.path.join(os.getenv('STORING'), 'logs', 'autoencoder_{}'.format(int(time.time())))
     supervisor = tf.train.Supervisor(logdir=logdir, summary_op=None)
     with supervisor.managed_session() as sess:
-        summary_writer = tf.summary.FileWriter(logdir)
-
         print('Training model with {} parameters...'.format(train_model.num_parameters))
         with tqdm(total=FLAGS.num_epochs) as pbar:
             for epoch in range(FLAGS.num_epochs):
+                summary_writer = tf.summary.FileWriter(logdir, sess.graph)
+
                 X_train_epoch = shuffle(X_train)
-                num_batches = len(X_train_epoch) // FLAGS.batch_size
 
                 losses = []
-                for batch_index in range(num_batches):
-                    batch_start = batch_index * FLAGS.batch_size
-                    batch_end = batch_start + FLAGS.batch_size
+                _, loss = sess.run([
+                    train_model.train_step,
+                    train_model.loss,
+                ], feed_dict={
+                    features: X_train_epoch,
+                })
+                losses.append(loss)
 
-                    X_train_batch = X_train_epoch[batch_start:batch_end]
-
-                    _, loss = sess.run([
-                        train_model.train_step,
-                        train_model.loss,
-                    ], feed_dict={
-                        features: X_train_batch,
-                    })
-                    losses.append(loss)
                 loss_train = np.mean(losses)
 
                 loss_valid, summary_str = sess.run([
@@ -102,8 +135,6 @@ def main(_):
                 pbar.update()
 
         summary_writer.add_graph(sess.graph)
-        summary_writer.flush()
-        summary_writer.close()
 
         loss_valid = sess.run(test_model.loss, feed_dict={
             features: X_valid,
@@ -114,10 +145,30 @@ def main(_):
         z_valid = sess.run(test_model.z, feed_dict={ features: X_valid })
         z_test = sess.run(test_model.z, feed_dict={ features: X_test })
 
+        summary_writer.flush()
+        summary_writer.close()
+
         if FLAGS.denoise:
             np.savez(os.path.join(os.getenv('STORING'), 'denoising.npz'), z_train=z_train, z_valid=z_valid, z_test=z_test)
         else:
             np.savez(os.path.join(os.getenv('STORING'), 'autoencoder.npz'), z_train=z_train, z_valid=z_valid, z_test=z_test)
+
+    tf.reset_default_graph()
+    tf.Graph().as_default()
+    embedding_variable = tf.Variable(z_train, name='autoencoder_embedding')
+    summary_writer = tf.summary.FileWriter(logdir)
+    config = projector.ProjectorConfig()
+    embedding = config.embeddings.add()
+    embedding.tensor_name = embedding_variable.name
+    embedding.metadata_path = os.path.join(logdir, 'metadata.tsv')
+    embedding.sprite.image_path = os.path.join(logdir, 'sprites.png')
+    embedding.sprite.single_image_dim.extend(dimensions)
+    projector.visualize_embeddings(summary_writer, config)
+    session = tf.InteractiveSession()
+    session.run(tf.global_variables_initializer())
+    saver = tf.train.Saver()
+    saver.save(session, os.path.join(logdir, 'model.ckpt'), 0)
+
 
 if __name__ == "__main__":
     tf.app.run()
